1ec4dd2108e52a61b815df9a065704e4f6defc09
Dennis Karlsson <30557440+Ghilea@users.noreply.github.com>
2021-11-19 23:32:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+//music function
+let bgMusic = paused => {
+
+    if (paused) {
+        audio.pause();
+        return;
+    }
+
+    const link = './sound/intro.mp3';
+    let audio = new Audio(link);
+    audio.volume = 0.5;
+    audio.loop = true;
+
+    audio.play();
+}
+
+//start the game
+document.querySelector('.start').addEventListener('click', () => {
+    
+    let scen = document.querySelector('.scen');
+
+    console.log(scen.style.display);
+    if(scen.style.display !== 'grid'){
+       setTimeout(() => {
+        bgMusic(false);
+    }, 3000); 
+
+     scen.style.display = 'grid';
+    }
+    
+
+})
